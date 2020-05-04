8cce07e4beebcd008ea2dc9c7a986b3392bbdc08
atakehiro <57095785+atakehiro@users.noreply.github.com>
2020-05-04 16:47:40 +0900

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python
 
+import tensorflow as tf
 import tifffile
 import numpy as np
-from unet import unet_model_3d
-
-def data_gen(image, window_size):
-    size = image.shape
-    mod0 = size[0] % window_size[0]
-    mod1 = size[1] % window_size[1]
-    mod2 = size[2] % window_size[2]
-    tmp0 = np.delete(image, list(range(image.shape[0] - mod0,image.shape[0])), axis=0)
-    tmp1 = np.delete(tmp0, list(range(image.shape[1] - mod1,image.shape[1])), axis=1)
-    tmp2 = np.delete(tmp1, list(range(image.shape[2] - mod2,image.shape[2])), axis=2)
-    data = tmp2.reshape((-1,1)+window_size)
-    return data
+from functions import unet_model_3d, data_gen
 
 if __name__ == "__main__":
     image = tifffile.imread('traindata/training_input.tif')
     label = tifffile.imread('traindata/training_groundtruth.tif')
-    res = 48
+    res = 48  # 8*n
     window_size = (res, res, res)
     input_data = data_gen(image, window_size)
     label_data = data_gen(label, window_size)
@@ -27,10 +17,8 @@ if __name__ == "__main__":
     model = unet_model_3d((1,) + window_size)
     model.summary()
     batch_size = 8
-    no_epochs = 10
+    no_epochs = 1
     model.fit(input_data, label_data, batch_size=batch_size, epochs=no_epochs, verbose=1)
     pred = model.predict(input_data)
-    #iou = mean_iou(label_data, (pred > 0.5).int())
-    #print('IOU:', iou)
     np.save('prediction', pred)
 
--- Main_LMS.py
+++ Main_LMS.py
@@ -11,33 +11,39 @@ K.set_session(tf.Session(config=config))
 from tensorflow_large_model_support import LMSKerasCallback
 lms_callback = LMSKerasCallback()
 
+"""
+from tensorflow.python.keras.callbacks import Callback
+class CudaProfileCallback(Callback):
+    def __init__(self, profile_epoch, profile_batch_start, profile_batch_end):
+        self._epoch = profile_epoch - 1
+        self._start = profile_batch_start
+        self._end = profile_batch_end
+        self.epoch_keeper = 0
+    def on_epoch_begin(self, epoch, logs=None):
+        self.epoch_keeper = epoch
+    def on_batch_begin(self, batch, logs=None):
+        if batch == self._start and self.epoch_keeper == self._epoch:
+            print('Starting cuda profiler')
+            _cudart.cudaProfilerStart()
+        if batch == self._end and self.epoch_keeper == self._epoch:
+            print('Stopping cuda profiler')
+            _cudart.cudaProfilerStop()
+callbacks = []
+callbacks.append(CudaProfileCallback(1, 4, 9))
+from tensorflow_large_model_support import LMSKerasCallback
+starting_names = ['up_sampling3d_2/concat_2']  #['bn_conv1/cond/pred_id']
+lms = LMSKerasCallback(n_tensors=-1, lb=1, starting_op_names=starting_names)
+callbacks.append(lms)
+"""
+
 import tifffile
 import numpy as np
-from unet import unet_model_3d
-
-def data_gen(image, window_size):
-    size = image.shape
-    mod0 = size[0] % window_size[0]
-    mod1 = size[1] % window_size[1]
-    mod2 = size[2] % window_size[2]
-    tmp0 = np.delete(image, list(range(image.shape[0] - mod0,image.shape[0])), axis=0)
-    tmp1 = np.delete(tmp0, list(range(image.shape[1] - mod1,image.shape[1])), axis=1)
-    tmp2 = np.delete(tmp1, list(range(image.shape[2] - mod2,image.shape[2])), axis=2)
-    data = tmp2.reshape((-1,1)+window_size)
-    return data
-
-def get_callbacks(args):
-    callbacks = []
-    #callbacks.append(CudaProfileCallback(args.nvprof_epoch, args.nvprof_start, args.nvprof_stop))
-    starting_names = ['bn_conv1/cond/pred_id']
-    lms = LMSKerasCallback(n_tensors=args.n_tensors, lb=args.lb, starting_op_names=starting_names)
-    callbacks.append(lms)
-    return callbacks
+from functions import unet_model_3d, data_gen
 
 if __name__ == "__main__":
     image = tifffile.imread('traindata/training_input.tif')
     label = tifffile.imread('traindata/training_groundtruth.tif')
-    res = 48 # 8*n
+    res = 56  # 8*n
     window_size = (res, res, res)
     input_data = data_gen(image, window_size)
     label_data = data_gen(label, window_size)
@@ -46,10 +52,7 @@ if __name__ == "__main__":
     model = unet_model_3d((1,) + window_size)
     model.summary()
     batch_size = 8
-    no_epochs = 10
+    no_epochs = 1
     model.fit(input_data, label_data, batch_size=batch_size, epochs=no_epochs, verbose=1, callbacks=[lms_callback])
     pred = model.predict(input_data)
-    #iou = mean_iou(label_data, (pred > 0.5).int())
-    #print('IOU:', iou)
     np.save('prediction', pred)
-
