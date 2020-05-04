ea9d0fa5b0c8542d5955058fbed6c733f355548d
atakehiro <57095785+atakehiro@users.noreply.github.com>
2020-05-04 16:58:59 +0900

    Update Main_LMS.py

--- Main_LMS.py
+++ Main_LMS.py
@@ -11,31 +11,6 @@ K.set_session(tf.Session(config=config))
 from tensorflow_large_model_support import LMSKerasCallback
 lms_callback = LMSKerasCallback()
 
-"""
-from tensorflow.python.keras.callbacks import Callback
-class CudaProfileCallback(Callback):
-    def __init__(self, profile_epoch, profile_batch_start, profile_batch_end):
-        self._epoch = profile_epoch - 1
-        self._start = profile_batch_start
-        self._end = profile_batch_end
-        self.epoch_keeper = 0
-    def on_epoch_begin(self, epoch, logs=None):
-        self.epoch_keeper = epoch
-    def on_batch_begin(self, batch, logs=None):
-        if batch == self._start and self.epoch_keeper == self._epoch:
-            print('Starting cuda profiler')
-            _cudart.cudaProfilerStart()
-        if batch == self._end and self.epoch_keeper == self._epoch:
-            print('Stopping cuda profiler')
-            _cudart.cudaProfilerStop()
-callbacks = []
-callbacks.append(CudaProfileCallback(1, 4, 9))
-from tensorflow_large_model_support import LMSKerasCallback
-starting_names = ['up_sampling3d_2/concat_2']  #['bn_conv1/cond/pred_id']
-lms = LMSKerasCallback(n_tensors=-1, lb=1, starting_op_names=starting_names)
-callbacks.append(lms)
-"""
-
 import tifffile
 import numpy as np
 from functions import unet_model_3d, data_gen
